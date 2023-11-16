da8534b4d2180fd6d81ec18f4c45a01f332a986b
Autumn Bement <aube7332@colorado.edu>
2023-11-16 11:01:23 -0700

    ICA12
    ICA12

---
+++ ica/ICA12/ica12.js
@@ -0,0 +1,28 @@
+const newBtn = document.querySelector('#js-new-quote').addEventListener('click', getQuote);
+
+const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
+
+async function getQuote() {
+   // console.log('Test');
+
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw Error(response.statusTexxt);
+        }
+        const json = await response.json();
+        //console.log(json);
+        displayQuote(json['question'])
+    }
+    catch(err){
+        console.log(err);
+        alert('Failed to fetch new quote');
+    }
+}
+
+function displayQuote(question) {
+    const questinonTxt = document.querySelector('#js-quote-text');
+    questinonTxt.textContent = question;
+}
+
+getQuote();
